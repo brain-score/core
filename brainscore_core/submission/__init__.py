83ebc1094c6f41267ee6c2cfddfdad080730a643
Martin Schrimpf <m4rtinsch@gmail.com>
2022-10-13 11:22:58 -0400

    setup trigger function signatures

--- brainscore_core/submission/__init__.py
+++ brainscore_core/submission/__init__.py
@@ -1,3 +1,28 @@
 """
-Score model submissions on Brain-Score benchmarks.
+Process plugin submissions (data, metrics, benchmarks, models) and score models on benchmarks.
 """
+from typing import List
+
+
+def process_zip_submission(zip_filepath: str):
+    """
+    Triggered when a zip file is submitted via the website.
+    Opens a pull request on GitHub with the plugin contents of the zip file.
+    The merge of this PR will potentially trigger `process_github_submission`.
+    """
+    pass  # TODO @Katherine
+
+
+def process_github_submission():
+    """
+    Triggered when changed are merged to the GitHub repository, if those changes affect benchmarks or models.
+    Starts parallel runs to score models on benchmarks (`run_scoring`).
+    """
+    pass  # TODO @Katherine
+
+
+def run_scoring(models: List[str], benchmarks: List[str]):
+    """
+    Run the `models` on the `benchmarks`, and write resulting scores to the database.
+    """
+    pass  # TODO @Katherine
