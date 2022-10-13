504d14695dd9532c3b49b00d91a45d695ec22d95
Martin Schrimpf <m4rtinsch@gmail.com>
2022-10-13 12:24:24 -0400

    add parent to Benchmark interface

--- brainscore_core/benchmarks/__init__.py
+++ brainscore_core/benchmarks/__init__.py
@@ -17,24 +17,26 @@ class Benchmark(ABC):
 
     def __call__(self, candidate):
         """
-        Evaluate a candidate `BrainModel` and return a :class:`~brainscore.metrics.Score` denoting the brain-likeness of
-        the model under this benchmark. Typically this involves reproducing the experiment on the model and then
-        comparing model measurements (e.g. neural/behavioral) against recordings from biological subjects (e.g.
-        primates) using a :class:`~brainscore.metrics.Metric`. The output of this method is a normalized score between 0
-        and 1 where 0 means the model does not match the measurements at all and 1 means the model matches the
-        measurements at ceiling level (e.g. if the model obtains a score of 0.8 and the data ceiling is also 0.8, the
-        score output by this method should be 1).
+        Evaluate a candidate `BrainModel` and return a :class:`~brainscore_core.metrics.Score` denoting the
+        alignment the model to natural intelligence under this benchmark.
+        Typically this involves reproducing the experiment on the model and then comparing model measurements
+        (e.g. neural/behavioral) against recordings from biological subjects (e.g. primates)
+        using a :class:`~brainscore_core.metrics.Metric`.
+        The output of this method is a normalized score between 0 and 1 where 0 means the model does not match the
+        measurements at all and 1 means the model matches the measurements at ceiling level
+        (e.g. if the model obtains a score of 0.8 and the data ceiling is also 0.8,
+        the score output by this method should be 1).
 
         :param candidate: a candidate model implementing the `BrainModel` interface. Benchmarks are agnostic of the
                 exact implementation and only interact with models through the methods defined in the interface.
-        :return: a :class:`~brainscore.metrics.Score` of how brain-like the candidate model is under this benchmark. The
-                score is normalized by this benchmark's ceiling such that 1 means the model matches the data to ceiling
-                level.
+        :return: a :class:`~brainscore_core.metrics.Score` of how brain-like the candidate model is under this
+                benchmark. The score is normalized by this benchmark's ceiling such that 1 means the model matches the
+                data to ceiling level.
         """
         raise NotImplementedError()
 
     @property
-    def bibtex(self):
+    def bibtex(self) -> str:
         """
         bibtex string to build the reference.
         Should include an `url` to build a proper link.
@@ -42,7 +44,7 @@ class Benchmark(ABC):
         raise NotImplementedError()
 
     @property
-    def identifier(self):
+    def identifier(self) -> str:
         """
         Unique identifier for this benchmark.
         Standard format is `<data identifier>-<metric identifier>`, e.g. `dicarlo.Rajalingham2018-i2n`.
@@ -52,17 +54,26 @@ class Benchmark(ABC):
         raise NotImplementedError()
 
     @property
+    def parent(self) -> str:
+        """
+        The identifier for the parent of this benchmark. Typically this is one of `behavioral`, `neural`, `engineering`.
+
+        For benchmarks composed of sub-benchmarks, the sub-benchmark's parent can also be an aggregate benchmark
+        identifier; for instance the sub-benchmarks 'Geirhos2021colour-error_consistency' and
+        'Geirhos2021contrast-error_consistency' might have 'Geirhos2021-error_consistency' as parent.
+        """
+        raise NotImplementedError()
+
+    @property
     def version(self):
         """
-        Optional, but strongly encouraged.
-
         :return: a version number that is increased every time the model scores for this benchmark change
                 (but not for code changes that do not change scores).
         """
         raise NotImplementedError()
 
     @property
-    def ceiling(self):
+    def ceiling(self) -> Score:
         """
         The ceiling of this benchmark. Scores need to be normalized by this value.
         Typically this represents the signal in the data and how well we expect the best possible model to score.
@@ -76,14 +87,14 @@ class Benchmark(ABC):
 
 class BenchmarkBase(Benchmark):
     """
-    Helper class for implementing standard functions of the `Benchmark` interface.
+    Helper class for implementing standard functions of the :class:`~brainscore_core.benchmarks.Benchmark` interface.
     """
 
-    def __init__(self, identifier, ceiling, version, parent=None, bibtex=None):
+    def __init__(self, identifier: str, ceiling: Score, version, parent: str, bibtex: str = None):
         self._identifier = identifier
         self._ceiling = ceiling
         self._version = version
-        self.parent = parent
+        self._parent = parent
         self._bibtex = bibtex
 
     @property
@@ -93,6 +104,10 @@ class BenchmarkBase(Benchmark):
     @property
     def identifier(self):
         return self._identifier
+
+    @property
+    def parent(self):
+        return self._parent
 
     @property
     def version(self):
