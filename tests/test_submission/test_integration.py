a749df540ec1aad5e05569f6044098577bfa5b03
Katherine Fairchild <kvfairchild@Victorias-Air.localdomain.chcnet>
2022-11-09 08:49:23 -0500

    replace pytest skip with pass

--- tests/test_submission/test_integration.py
+++ tests/test_submission/test_integration.py
@@ -5,12 +5,13 @@ import pytest
 from datetime import datetime
 from pytest import approx
 
-@pytest.mark.skip(reason="submission.evaluation is deprecated and we're not ready to rewrite integration tests yet", allow_module_level=True)
-
 from brainscore_core.submission.database import connect_db
 from brainscore_core.submission.database_models import Score, Model, Submission
-from brainscore_core.submission.evaluation import run_evaluation
-from tests.test_submission.test_db import clear_schema, init_user, init_benchmark_parents
+try:
+	from brainscore_core.submission.evaluation import run_evaluation
+	from tests.test_submission.test_db import clear_schema, init_user, init_benchmark_parents
+except:
+	pass # these are deprecated and we're not ready to rewrite integration tests yet
 
 logger = logging.getLogger(__name__)
 
@@ -26,107 +27,107 @@ logger = logging.getLogger(__name__)
 @pytest.mark.memory_intense
 @pytest.mark.private_access
 class TestIntegration:
-    database = 'brainscore-ohio-test'  # test database
+	database = 'brainscore-ohio-test'  # test database
 
-    @classmethod
-    def setup_class(cls):
-        logger.info('Connect to database')
-        connect_db(TestIntegration.database)
-        clear_schema()
+	@classmethod
+	def setup_class(cls):
+		logger.info('Connect to database')
+		connect_db(TestIntegration.database)
+		clear_schema()
 
-    def setup_method(self):
-        logger.info('Initialize database')
-        init_user()
-        init_benchmark_parents()
+	def setup_method(self):
+		logger.info('Initialize database')
+		init_user()
+		init_benchmark_parents()
 
-    def teardown_method(self):
-        logger.info('Clean database')
-        clear_schema()
+	def teardown_method(self):
+		logger.info('Clean database')
+		clear_schema()
 
-    def test_competition_field(self, tmpdir):
-        working_dir = str(tmpdir.mkdir('sub'))
-        config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
-        run_evaluation(config_dir, working_dir, 33, TestIntegration.database, models=['alexnet'],
-                       benchmarks=['dicarlo.MajajHong2015.IT-pls'])
-        model = Model.get()
-        assert model.competition == "cosyne2022"
+	def test_competition_field(self, tmpdir):
+		working_dir = str(tmpdir.mkdir('sub'))
+		config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
+		run_evaluation(config_dir, working_dir, 33, TestIntegration.database, models=['alexnet'],
+					   benchmarks=['dicarlo.MajajHong2015.IT-pls'])
+		model = Model.get()
+		assert model.competition == "cosyne2022"
 
-    def test_competition_field_none(self, tmpdir):
-        working_dir = str(tmpdir.mkdir('sub'))
-        config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
-        submission = Submission.create(id=33, submitter=1, timestamp=datetime.now(),
-                                       model_type='BaseModel', status='running')
-        model = Model.create(name='alexnet', owner=submission.submitter, public=False,
-                             submission=submission)
-        with open(f'{config_dir}submission_40.json', 'w') as rerun:
-            rerun.write(f"""{{
-                "model_ids": [{model.id}], "user_id": 1, "competition": null}}""")
-        run_evaluation(config_dir, working_dir, 40, TestIntegration.database,
-                       benchmarks=['dicarlo.Rajalingham2018-i2n'])
-        model = Model.get()
-        assert model.competition is None
+	def test_competition_field_none(self, tmpdir):
+		working_dir = str(tmpdir.mkdir('sub'))
+		config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
+		submission = Submission.create(id=33, submitter=1, timestamp=datetime.now(),
+									   model_type='BaseModel', status='running')
+		model = Model.create(name='alexnet', owner=submission.submitter, public=False,
+							 submission=submission)
+		with open(f'{config_dir}submission_40.json', 'w') as rerun:
+			rerun.write(f"""{{
+				"model_ids": [{model.id}], "user_id": 1, "competition": null}}""")
+		run_evaluation(config_dir, working_dir, 40, TestIntegration.database,
+					   benchmarks=['dicarlo.Rajalingham2018-i2n'])
+		model = Model.get()
+		assert model.competition is None
 
-    def test_evaluation(self, tmpdir):
-        working_dir = str(tmpdir.mkdir('sub'))
-        config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
-        run_evaluation(config_dir, working_dir, 33, TestIntegration.database, models=['alexnet'],
-                       benchmarks=['dicarlo.MajajHong2015.IT-pls'])
-        with open('result_33.csv') as results:
-            csv_reader = csv.reader(results, delimiter=',')
-            next(csv_reader)  # header row
-            result_row = next(csv_reader)
-            assert result_row[0] == 'alexnet'
-            assert result_row[1] == 'dicarlo.MajajHong2015.IT-pls'
-            assert float(result_row[2]) == approx(0.5857491098187586, abs=0.0001)
-            assert float(result_row[3]) == approx(0.5079816726934638, abs=0.0001)
-            assert float(result_row[4]) == approx(0.003155449372125895, abs=0.0001)
-        scores = Score.select()
-        assert len(scores) == 1
-        # successful score comment should inform about which layers were used for which regions
-        assert scores[0].comment.startswith("layers:")
+	def test_evaluation(self, tmpdir):
+		working_dir = str(tmpdir.mkdir('sub'))
+		config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
+		run_evaluation(config_dir, working_dir, 33, TestIntegration.database, models=['alexnet'],
+					   benchmarks=['dicarlo.MajajHong2015.IT-pls'])
+		with open('result_33.csv') as results:
+			csv_reader = csv.reader(results, delimiter=',')
+			next(csv_reader)  # header row
+			result_row = next(csv_reader)
+			assert result_row[0] == 'alexnet'
+			assert result_row[1] == 'dicarlo.MajajHong2015.IT-pls'
+			assert float(result_row[2]) == approx(0.5857491098187586, abs=0.0001)
+			assert float(result_row[3]) == approx(0.5079816726934638, abs=0.0001)
+			assert float(result_row[4]) == approx(0.003155449372125895, abs=0.0001)
+		scores = Score.select()
+		assert len(scores) == 1
+		# successful score comment should inform about which layers were used for which regions
+		assert scores[0].comment.startswith("layers:")
 
-    def test_rerun_evaluation(self, tmpdir):
-        working_dir = str(tmpdir.mkdir('sub'))
-        config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
-        submission = Submission.create(id=33, submitter=1, timestamp=datetime.now(),
-                                       model_type='BaseModel', status='running')
-        model = Model.create(name='alexnet', owner=submission.submitter, public=False,
-                             submission=submission)
-        with open(f'{config_dir}submission_34.json', 'w') as rerun:
-            rerun.write(f"""{{
-            "model_ids": [{model.id}], "user_id": 1}}""")
-        run_evaluation(config_dir, working_dir, 34, TestIntegration.database,
-                       benchmarks=['dicarlo.Rajalingham2018-i2n'])
-        with open('result_34.csv') as results:
-            csv_reader = csv.reader(results, delimiter=',')
-            next(csv_reader)  # header row
-            result_row = next(csv_reader)
-            assert result_row[0] == 'alexnet'
-            assert result_row[1] == 'dicarlo.Rajalingham2018-i2n'
-            assert float(result_row[2]) == approx(0.25771746331458695, abs=0.0001)
-            assert float(result_row[3]) == approx(0.3701702418190641, abs=0.0001)
-            assert float(result_row[4]) == approx(0.011129032024657565, abs=0.0001)
+	def test_rerun_evaluation(self, tmpdir):
+		working_dir = str(tmpdir.mkdir('sub'))
+		config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
+		submission = Submission.create(id=33, submitter=1, timestamp=datetime.now(),
+									   model_type='BaseModel', status='running')
+		model = Model.create(name='alexnet', owner=submission.submitter, public=False,
+							 submission=submission)
+		with open(f'{config_dir}submission_34.json', 'w') as rerun:
+			rerun.write(f"""{{
+			"model_ids": [{model.id}], "user_id": 1}}""")
+		run_evaluation(config_dir, working_dir, 34, TestIntegration.database,
+					   benchmarks=['dicarlo.Rajalingham2018-i2n'])
+		with open('result_34.csv') as results:
+			csv_reader = csv.reader(results, delimiter=',')
+			next(csv_reader)  # header row
+			result_row = next(csv_reader)
+			assert result_row[0] == 'alexnet'
+			assert result_row[1] == 'dicarlo.Rajalingham2018-i2n'
+			assert float(result_row[2]) == approx(0.25771746331458695, abs=0.0001)
+			assert float(result_row[3]) == approx(0.3701702418190641, abs=0.0001)
+			assert float(result_row[4]) == approx(0.011129032024657565, abs=0.0001)
 
-    def test_failure_evaluation(self, tmpdir):
-        working_dir = str(tmpdir.mkdir('sub'))
-        config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
-        with pytest.raises(Exception):
-            run_evaluation(config_dir, working_dir, 35, TestIntegration.database, models=['alexnet'],
-                           benchmarks=['dicarlo.Rajalingham2018-i2n'])
+	def test_failure_evaluation(self, tmpdir):
+		working_dir = str(tmpdir.mkdir('sub'))
+		config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
+		with pytest.raises(Exception):
+			run_evaluation(config_dir, working_dir, 35, TestIntegration.database, models=['alexnet'],
+						   benchmarks=['dicarlo.Rajalingham2018-i2n'])
 
-    def test_model_failure_evaluation(self, tmpdir):
-        working_dir = str(tmpdir.mkdir('sub'))
-        config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
-        run_evaluation(config_dir, working_dir, 36, TestIntegration.database, models=['alexnet'],
-                       benchmarks=['movshon.FreemanZiemba2013.V1-pls'])
-        with open('result_36.csv') as results:
-            csv_reader = csv.reader(results, delimiter=',')
-            next(csv_reader)  # header row
-            result_row = next(csv_reader)
-            assert result_row[0] == 'alexnet'
-            assert result_row[1] == 'movshon.FreemanZiemba2013.V1-pls'
-            assert result_row[2] == '0'
-            assert result_row[3] == '0'
-        model = Model.get()
-        score = Score.get(model=model)
-        assert score.comment is not None  # When there's a problem, the comment field contains an error message
+	def test_model_failure_evaluation(self, tmpdir):
+		working_dir = str(tmpdir.mkdir('sub'))
+		config_dir = str(os.path.join(os.path.dirname(__file__), 'configs/'))
+		run_evaluation(config_dir, working_dir, 36, TestIntegration.database, models=['alexnet'],
+					   benchmarks=['movshon.FreemanZiemba2013.V1-pls'])
+		with open('result_36.csv') as results:
+			csv_reader = csv.reader(results, delimiter=',')
+			next(csv_reader)  # header row
+			result_row = next(csv_reader)
+			assert result_row[0] == 'alexnet'
+			assert result_row[1] == 'movshon.FreemanZiemba2013.V1-pls'
+			assert result_row[2] == '0'
+			assert result_row[3] == '0'
+		model = Model.get()
+		score = Score.get(model=model)
+		assert score.comment is not None  # When there's a problem, the comment field contains an error message
