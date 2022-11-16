7f5b86d3499d5b1d392c3555f9e3e5d8591c69ad
Martin Schrimpf <m4rtinsch@gmail.com>
2022-11-15 23:09:03 -0500

    copy local files rather than `f.write` for `test_import_plugin`

--- tests/test_plugin_management/test_import_plugin.py
+++ tests/test_plugin_management/test_import_plugin.py
@@ -3,59 +3,19 @@ import shutil
 import subprocess
 import sys
 import tempfile
-import textwrap
 from pathlib import Path
 
 from brainscore_core.plugin_management.import_plugin import import_plugin
 
 dummy_container_dirpath = Path(tempfile.mkdtemp("brainscore-dummy"))
-dummy_domain_dirpath = dummy_container_dirpath / "brainscore_dummy"
-dummy_model_dirpath = dummy_domain_dirpath / "models" / "dummy_model"
 current_dependencies_pref = os.getenv('BS_INSTALL_DEPENDENCIES')
 
 
 class TestImportPlugin:
     def setup_method(self):
-        dummy_registry = dummy_domain_dirpath / "__init__.py"
-        dummy_model = dummy_model_dirpath / "model.py"
-        dummy_testfile = dummy_model_dirpath / "test.py"
-        dummy_requirements = dummy_model_dirpath / "test.py"
-        dummy_init = dummy_model_dirpath / "__init__.py"
-
-        dummy_model_dirpath.mkdir(parents=True)
         sys.path.append(str(dummy_container_dirpath))
-
-        Path(dummy_registry).touch()
-        with open(dummy_registry, 'w') as f:
-            f.write(textwrap.dedent('''\
-            model_registry = {}
-            '''))
-
-        Path(dummy_model).touch()
-        with open(dummy_model, 'w') as f:
-            f.write(textwrap.dedent('''\
-            class dummyModel:
-                pass       
-            '''))
-        Path(dummy_testfile).touch()
-        with open(dummy_testfile, 'w') as f:
-            f.write(textwrap.dedent('''\
-            def test_dummy():
-                assert True        
-            '''))
-        Path(dummy_requirements).touch()
-        with open(dummy_requirements, 'w') as f:
-            f.write(textwrap.dedent('''\
-            pyaztro     
-            '''))
-        Path(dummy_init).touch()
-        with open(dummy_init, 'w') as f:
-            f.write(textwrap.dedent('''\
-            from brainscore_dummy import model_registry
-            from .model import dummyModel
-
-            model_registry['dummy-model'] = dummyModel     
-            '''))
+        local_resource = Path(__file__).parent / 'test_import_plugin__brainscore_dummy'  # dummy-library scripts
+        shutil.copytree(local_resource / 'brainscore_dummy', dummy_container_dirpath / 'brainscore_dummy')
 
     def teardown_method(self):
         model_registry = self._model_registry()
---
+++ tests/test_plugin_management/test_import_plugin__brainscore_dummy/brainscore_dummy/models/dummy_model/model.py
@@ -0,0 +1,2 @@
+class DummyModel:
+    pass
