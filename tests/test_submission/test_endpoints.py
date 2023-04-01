1581b6e8be1bdf3353cb6012b4360272b3748bfd
Martin Schrimpf <m4rtinsch@gmail.com>
2023-04-01 12:56:00 -0400

    add improved `shorten_text` method for error message comment

--- tests/test_submission/test_endpoints.py
+++ tests/test_submission/test_endpoints.py
@@ -1,13 +1,14 @@
 from collections import namedtuple
 
 import logging
+import sys
 
 from brainscore_core import Score, Benchmark
-from brainscore_core.submission.database import connect_db
-from brainscore_core.submission.endpoints import RunScoringEndpoint, DomainPlugins
-from tests.test_submission import init_users
-from brainscore_core.submission.database_models import clear_schema
 from brainscore_core.submission import database_models
+from brainscore_core.submission.database import connect_db
+from brainscore_core.submission.database_models import clear_schema
+from brainscore_core.submission.endpoints import RunScoringEndpoint, DomainPlugins, shorten_text
+from tests.test_submission import init_users
 
 test_database = 'brainscore-ohio-test'
 
@@ -53,3 +54,35 @@ class TestRunScoring:
         assert len(score_entries) == 1
         score_entry = score_entries[0]
         assert score_entry.score_raw == 0.8
+
+
+class TestShortenText:
+    def test_text_short_enough(self):
+        text = 'lorem ipsum dolor sit amet'
+        shortened = shorten_text(text, max_length=30)
+        assert shortened == text
+
+    def test_text_exact_size(self):
+        text = 'lorem ipsum dolor sit amet'
+        shortened = shorten_text(text, max_length=len(text))
+        assert shortened == text
+
+    def test_text_too_long(self):
+        text = 'lorem ipsum dolor sit amet'
+        shortened = shorten_text(text, max_length=20)
+        assert len(shortened) == 20
+        assert shortened == 'lorem[...]r sit amet'
+
+    def test_text_too_long_by_1(self):
+        text = 'lorem ipsum dolor sit amet'
+        max_length = len(text) - 1
+        shortened = shorten_text(text, max_length=max_length)
+        assert len(shortened) == max_length
+        assert shortened == 'lorem i[...]olor sit amet'
+
+    def test_text_too_long_by_2(self):
+        text = 'lorem ipsum dolor sit amet'
+        max_length = len(text) - 2
+        shortened = shorten_text(text, max_length=max_length)
+        assert len(shortened) == max_length
+        assert shortened == 'lorem i[...]lor sit amet'
