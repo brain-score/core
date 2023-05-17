9e38e206076527cd6d6f3557d4d1484dee76d857
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2023-05-17 11:45:40 -0400

    Add support for non-Python models and benchmarks via `environment.yml` (#31)
    * refactor https://github.com/brain-score/language/pull/113 into core
    
    * add fire dependency
    
    * explain environment 'name' restriction
    
    Co-authored-by: kvfairchild <kvg0@mit.edu>
    
    * fix `not` conditional
    
    * simplify naming
    
    * avoid redundancy
    
    ---------
    
    Co-authored-by: kvfairchild <kvg0@mit.edu>

--- tests/test_plugin_management/test_test_plugins.py
+++ tests/test_plugin_management/test_test_plugins.py
@@ -1,61 +1,66 @@
+import pytest
 import shutil
 import subprocess
 import tempfile
 from pathlib import Path
-
-import pytest
 
 from brainscore_core.plugin_management.test_plugins import PluginTestRunner
 
 
 class TestPluginTestRunner:
     def setup_method(self):
-        self.dummy_library_path = Path(tempfile.mkdtemp("plugin-library"))
-        self.dummy_plugin_path = self.dummy_library_path / 'brainscore_dummy' / 'plugintype' / 'pluginname'
+        self.library_path = Path(tempfile.mkdtemp("plugin-library"))
         local_resource = Path(__file__).parent / 'test_test_plugins__brainscore_dummy'
         # `shutil.copytree(..., dirs_exist_ok=True)` would be preferable here but is not available in python 3.7
-        subprocess.run(f"cp -r {local_resource}/* {self.dummy_library_path}", shell=True, text=True, check=True)
+        subprocess.run(f"cp -r {local_resource}/* {self.library_path}", shell=True, text=True, check=True)
 
     def teardown_method(self):
-        shutil.rmtree(self.dummy_library_path)
+        shutil.rmtree(self.library_path)
 
     def test_plugin_name(self):
-        plugin_test_runner = PluginTestRunner(self.dummy_plugin_path, {})
-        assert plugin_test_runner.plugin_name == 'plugintype__pluginname'
+        dummy_plugin_path = self.library_path / 'brainscore_dummy' / 'plugintype' / 'dummy_plugin'
+        plugin_test_runner = PluginTestRunner(dummy_plugin_path, {})
+        assert plugin_test_runner.plugin_name == 'plugintype__dummy_plugin'
 
     def test_has_no_testfile(self):
-        test_file = self.dummy_plugin_path / 'test.py'
+        dummy_plugin_path = self.library_path / 'brainscore_dummy' / 'plugintype' / 'dummy_plugin'
+        test_file = dummy_plugin_path / 'test.py'
         test_file.unlink()
-        plugin_test_runner = PluginTestRunner(self.dummy_plugin_path, {})
+        plugin_test_runner = PluginTestRunner(dummy_plugin_path, {})
         with pytest.raises(Exception):
             plugin_test_runner.validate_plugin()
 
     def test_has_testfile_underscore_prefix(self):
-        test_file = self.dummy_plugin_path / 'test.py'
-        test_file.rename(self.dummy_plugin_path / 'test_plugin.py')
-        plugin_test_runner = PluginTestRunner(self.dummy_plugin_path, {})
+        dummy_plugin_path = self.library_path / 'brainscore_dummy' / 'plugintype' / 'dummy_plugin'
+        test_file = dummy_plugin_path / 'test.py'
+        test_file.rename(dummy_plugin_path / 'test_plugin.py')
+        plugin_test_runner = PluginTestRunner(dummy_plugin_path, {})
         plugin_test_runner.validate_plugin()
 
     def test_has_testfile_no_underscore_prefix(self):
-        test_file = self.dummy_plugin_path / 'test.py'
-        test_file.rename(self.dummy_plugin_path / 'testplugin.py')
-        plugin_test_runner = PluginTestRunner(self.dummy_plugin_path, {})
+        dummy_plugin_path = self.library_path / 'brainscore_dummy' / 'plugintype' / 'dummy_plugin'
+        test_file = dummy_plugin_path / 'test.py'
+        test_file.rename(dummy_plugin_path / 'testplugin.py')
+        plugin_test_runner = PluginTestRunner(dummy_plugin_path, {})
         plugin_test_runner.validate_plugin()
-        
+
     def test_has_testfile_valid_python(self):
-        test_file = self.dummy_plugin_path / 'test.py'
-        test_file.rename(self.dummy_plugin_path / 'testpluginpy')
-        plugin_test_runner = PluginTestRunner(self.dummy_plugin_path, {})
+        dummy_plugin_path = self.library_path / 'brainscore_dummy' / 'plugintype' / 'dummy_plugin'
+        test_file = dummy_plugin_path / 'test.py'
+        test_file.rename(dummy_plugin_path / 'testpluginpy')
+        plugin_test_runner = PluginTestRunner(dummy_plugin_path, {})
         with pytest.raises(Exception):
             plugin_test_runner.validate_plugin()
 
-    def test_has_requirements(self):
-        requirements_file = self.dummy_plugin_path / 'requirements.txt'
-        requirements_file.unlink()
-        plugin_test_runner = PluginTestRunner(self.dummy_plugin_path, {})
-        assert not plugin_test_runner.has_requirements
-
     def test_run_tests(self):
-        plugin_test_runner = PluginTestRunner(self.dummy_plugin_path, {})
+        dummy_plugin_path = self.library_path / 'brainscore_dummy' / 'plugintype' / 'dummy_plugin'
+        plugin_test_runner = PluginTestRunner(dummy_plugin_path, {})
+        plugin_test_runner.run_tests()
+        assert plugin_test_runner.results[plugin_test_runner.plugin_name] == 0
+
+    @pytest.mark.travis_slow
+    def test_run_tests_with_r(self):
+        r_plugin_path = self.library_path / 'brainscore_dummy' / 'plugintype' / 'r_plugin'
+        plugin_test_runner = PluginTestRunner(r_plugin_path, {})
         plugin_test_runner.run_tests()
         assert plugin_test_runner.results[plugin_test_runner.plugin_name] == 0
