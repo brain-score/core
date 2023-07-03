ce47a1218101da4b5de4d65641a5e9dc6bd9810b
kvfairchild <kvg0@mit.edu>
2023-07-03 09:50:46 -0400

    Update tests/test_plugin_management/test_parse_plugin_changes__brainscore_dummy/benchmarks/dummy_benchmark/__init__.py
    Co-authored-by: Martin Schrimpf <mschrimpf@users.noreply.github.com>

--- tests/test_plugin_management/test_parse_plugin_changes__brainscore_dummy/benchmarks/dummy_benchmark/__init__.py
+++ tests/test_plugin_management/test_parse_plugin_changes__brainscore_dummy/benchmarks/dummy_benchmark/__init__.py
@@ -1,5 +1,5 @@
 from brainscore_dummy import benchmark_registry
 
-from .benchmark import DummyBenchmark
+from .benchmark import DummyBenchmark  # doesn't exist, but we only parse and never run this file and never run it
 
 benchmark_registry['dummy-benchmark'] = DummyBenchmark
