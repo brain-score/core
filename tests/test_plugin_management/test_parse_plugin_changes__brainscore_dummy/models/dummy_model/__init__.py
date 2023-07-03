abdfe12222c5a694942ee5b462a8f9e4d8f0c4d7
kvfairchild <kvg0@mit.edu>
2023-07-03 09:51:07 -0400

    Update tests/test_plugin_management/test_parse_plugin_changes__brainscore_dummy/models/dummy_model/__init__.py
    Co-authored-by: Martin Schrimpf <mschrimpf@users.noreply.github.com>

--- tests/test_plugin_management/test_parse_plugin_changes__brainscore_dummy/models/dummy_model/__init__.py
+++ tests/test_plugin_management/test_parse_plugin_changes__brainscore_dummy/models/dummy_model/__init__.py
@@ -1,5 +1,5 @@
 from brainscore_dummy import model_registry
 
-from .model import DummyModel
+from .model import DummyModel  # doesn't exist, but we only parse and never run this file
 
 model_registry['dummy-model'] = DummyModel
