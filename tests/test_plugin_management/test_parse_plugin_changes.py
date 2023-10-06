2a088521af25b2912a9cafd833be5b0561508171
Katherine Fairchild <kvfairchild@gmail.com>
2023-10-05 21:32:06 -0400

    cleanup

--- tests/test_plugin_management/test_parse_plugin_changes.py
+++ tests/test_plugin_management/test_parse_plugin_changes.py
@@ -5,7 +5,7 @@ import sys
 import tempfile
 from pathlib import Path
 
-from brainscore_core.plugin_management.parse_plugin_changes import get_all_changed_files, separate_plugin_files, get_plugin_paths, get_plugin_ids
+from brainscore_core.plugin_management.parse_plugin_changes import separate_plugin_files, get_plugin_paths, get_plugin_ids
 
 DUMMY_FILES_CHANGED = ['brainscore_core/models/dummy_model/model.py', 
                 'brainscore_core/models/dummy_model/test.py', 
@@ -33,7 +33,6 @@ def test_get_plugin_paths():
 
 
 def test_get_plugin_ids():
-
     dummy_root = str(Path(__file__).parent / 'test_parse_plugin_changes__brainscore_dummy')
     plugin_types = ['models', 'benchmarks']
     for plugin_type in plugin_types:
