ef79446c09dc27c266bfd491760bccf305face2d
Deirdre Kelliher <dmkelliher@gmail.com>
2024-05-02 18:26:36 -0400

    bash syntax error pt 2

--- brainscore_core/plugin_management/test_plugin.sh
+++ brainscore_core/plugin_management/test_plugin.sh
@@ -81,17 +81,17 @@ echo "PLUGIN_TEST_SUCCESS: $PLUGIN_TEST_SUCCESS"
 GENERIC_TEST_SUCCESS=0
 PLUGIN_TEST_SUCCESS=5
 (($GENERIC_TEST_SUCCESS == 0)) && echo "Generic tests succeeded" || echo "Generic tests failed, return code $GENERIC_TEST_SUCCESS"
-((PLUGIN_TEST_SUCCESS == 0 || PLUGIN_TEST_SUCCESS == 5)) && echo "Plugin-specific tests succeeded" || echo "Generic tests failed, return code $PLUGIN_TEST_SUCCESS"
+(($PLUGIN_TEST_SUCCESS == 0 || PLUGIN_TEST_SUCCESS == 5)) && echo "Plugin-specific tests succeeded" || echo "Generic tests failed, return code $PLUGIN_TEST_SUCCESS"
 
 GENERIC_TEST_SUCCESS=5
 PLUGIN_TEST_SUCCESS=0
 (($GENERIC_TEST_SUCCESS == 0)) && echo "Generic tests succeeded" || echo "Generic tests failed, return code $GENERIC_TEST_SUCCESS"
-((PLUGIN_TEST_SUCCESS == 0 || PLUGIN_TEST_SUCCESS == 5)) && echo "Plugin-specific tests succeeded" || echo "Generic tests failed, return code $PLUGIN_TEST_SUCCESS"
+(($PLUGIN_TEST_SUCCESS == 0 || PLUGIN_TEST_SUCCESS == 5)) && echo "Plugin-specific tests succeeded" || echo "Generic tests failed, return code $PLUGIN_TEST_SUCCESS"
 
 GENERIC_TEST_SUCCESS=0
 PLUGIN_TEST_SUCCESS=1
 (($GENERIC_TEST_SUCCESS == 0)) && echo "Generic tests succeeded" || echo "Generic tests failed, return code $GENERIC_TEST_SUCCESS"
-((PLUGIN_TEST_SUCCESS == 0 || PLUGIN_TEST_SUCCESS == 5))) && echo "Plugin-specific tests succeeded" || echo "Generic tests failed, return code $PLUGIN_TEST_SUCCESS"
+(($PLUGIN_TEST_SUCCESS == 0 || PLUGIN_TEST_SUCCESS == 5))) && echo "Plugin-specific tests succeeded" || echo "Generic tests failed, return code $PLUGIN_TEST_SUCCESS"
 
 
 if [ $GENERIC_TEST_SUCCESS -ne 0 ]; then
