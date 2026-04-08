c83fa9afe534025608baca53ba81621f49472730
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-04-08 13:39:48 -0400

    Export unified model interface types from brainscore_core

--- brainscore_core/__init__.py
+++ brainscore_core/__init__.py
@@ -1,2 +1,10 @@
 from .metrics import Metric, Score
 from .benchmarks import Benchmark
+from .model_interface import (
+    TaskContext,
+    UnifiedModel,
+    ModalityProcessor,
+    ModalityIntegrator,
+    BrainScoreModel,
+)
+from .compatibility import CompatibilityError, CompatibilityWarning, check_compatibility
