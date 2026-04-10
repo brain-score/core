885c6908d7861cd3a04d12bcebeb651f92bdc504
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-04-10 09:04:26 -0400

    init

--- brainscore_core/submission/database_models.py
+++ brainscore_core/submission/database_models.py
@@ -134,7 +134,7 @@ class BenchmarkDataMeta(PeeweeBase):
     pre_processing = CharField(max_length=100, null=True)
     brainscore_link = CharField(max_length=200, null=True)
     extra_notes = CharField(max_length=1000, null=True)
-    data_publicly_available = BooleanField(default=True, null=False)
+    data_publicly_available = BooleanField(default=False, null=False)
 
     class Meta:
         table_name = 'brainscore_benchmark_data_meta'
